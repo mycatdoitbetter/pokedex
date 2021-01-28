dee007392b3c417339d0b2cd0b746be1c068f225
André Castelo <andrecasteloecomp@gmail.com>
2021-01-28 13:35:11 -0300

    Structing Linter, Babel...

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Pokedex</title>
+      <link rel="icon" href="/public/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Hello My Friends</h1>
+    </main>
+  </div>
+)
+
+export default Home
