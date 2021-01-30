8c0b81752c5731b1da29095e888089e3f0461c89
André Santos Castelo <andrecasteloecomp@gmail.com>
2021-01-29 22:16:06 -0300

    Solving types default exporst

--- src/pages/utils/functions.ts
+++ src/pages/utils/functions.ts
@@ -1,3 +1,8 @@
+/* eslint-disable max-len */
 export const capitalizeFirstLetter = (string: string) : string => string.charAt(0).toUpperCase() + string.slice(1)
 
-export const zerosPrefix = (integer: number, quantityOfNumbers: number) => String(integer).padStart(quantityOfNumbers, '0')
+export const zerosPrefix = (integer: number, quantityOfNumbers: number) : string => String(integer).padStart(quantityOfNumbers, '0')
+
+const string = ''
+
+export default string
